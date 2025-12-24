9c1de88d491a66d97646f8abc5bac021c04ab26f
mfwolffe <wolffemf@dukes.jmu.edu>
2025-12-24 17:14:18 -0500

    fortty: update to v0.1.3 - Clean build (zero warnings)

--- Formula/fortty.rb
+++ Formula/fortty.rb
@@ -1,8 +1,8 @@
 class Fortty < Formula
   desc "GPU-accelerated terminal emulator written in Fortran"
   homepage "https://github.com/FortranGoingOnForty/fortty"
-  url "https://github.com/FortranGoingOnForty/fortty/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "5c8b2d9e331ad1f0985606ce9252f2fcceb94586d62f949d138048c28cea0749"
+  url "https://github.com/FortranGoingOnForty/fortty/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "20b39136cb6dd954e2aae48cafc3fd8d05aa068779d14dfdbb5f84c71ee388d9"
   license "MIT"
 
   depends_on "cmake" => :build
