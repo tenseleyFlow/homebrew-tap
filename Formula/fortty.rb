c92a90749ea175fd14bda0bb160e3788d3ae0794
mfwolffe <wolffemf@dukes.jmu.edu>
2025-12-24 16:56:48 -0500

    fortty: update to v0.1.2 - Complete Wayland compositor fix

--- Formula/fortty.rb
+++ Formula/fortty.rb
@@ -1,8 +1,8 @@
 class Fortty < Formula
   desc "GPU-accelerated terminal emulator written in Fortran"
   homepage "https://github.com/FortranGoingOnForty/fortty"
-  url "https://github.com/FortranGoingOnForty/fortty/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "f4ad65d740fe6bf6495224b053f87f4f5eea64a2f63f98f6dc343b27619a0df5"
+  url "https://github.com/FortranGoingOnForty/fortty/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "5c8b2d9e331ad1f0985606ce9252f2fcceb94586d62f949d138048c28cea0749"
   license "MIT"
 
   depends_on "cmake" => :build
