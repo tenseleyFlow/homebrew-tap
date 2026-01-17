a7c8d1eeb5dd0fa5caeef4e46c95a73a4b073114
mfwolffe <wolffemf@dukes.jmu.edu>
2026-01-17 10:49:41 -0500

    add gump formula

---
+++ Formula/gump.rb
@@ -0,0 +1,38 @@
+class Gump < Formula
+  desc "A smarter cd command - directory jumper using frecency"
+  homepage "https://github.com/tenseleyFlow/gump"
+  url "https://github.com/tenseleyFlow/gump/archive/refs/tags/v0.1.0.tar.gz"
+  sha256 "c6839e728a30ff1fc5ffeff60c1d11ee3b7e224161446b26ddf017402bf4c5b1"
+  license "MIT"
+  head "https://github.com/tenseleyFlow/gump.git", branch: "trunk"
+
+  depends_on "rust" => :build
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  def caveats
+    <<~EOS
+      Add to your shell config:
+
+      Bash (~/.bashrc):
+        eval "$(gump init bash)"
+
+      Zsh (~/.zshrc):
+        eval "$(gump init zsh)"
+
+      Fish (~/.config/fish/config.fish):
+        gump init fish | source
+
+      Usage:
+        g foo        # jump to best match
+        gi foo       # interactive with fzf
+        foo          # no prefix needed
+    EOS
+  end
+
+  test do
+    assert_match "gump", shell_output("#{bin}/gump --help")
+  end
+end
