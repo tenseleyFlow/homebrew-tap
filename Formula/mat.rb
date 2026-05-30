0334b87e642fb6e44fca5aa97183729ab9bfbb90
mfwolffe <mfwolffe@outlook.com>
2026-05-30 15:30:04 -0400

    mat 0.4.0

--- Formula/mat.rb
+++ Formula/mat.rb
@@ -2,8 +2,8 @@ class Mat < Formula
   desc "A fast cat/bat alternative with syntax highlighting and a built-in pager"
   homepage "https://github.com/tenseleyFlow/mat"
   url "https://github.com/tenseleyFlow/mat.git",
-      tag: "v0.3.0",
-      revision: "9420391a8e4cd51d51e76126d6a2c3b65db12b51"
+      tag: "v0.4.0",
+      revision: "611cb3a762b9a90af28524c7d51669433cae5b17"
   license "MIT"
   head "https://github.com/tenseleyFlow/mat.git", branch: "trunk"
 
@@ -18,7 +18,7 @@ class Mat < Formula
   end
 
   test do
-    assert_match "mat 0.3.0", shell_output("#{bin}/mat --version")
+    assert_match "mat 0.4.0", shell_output("#{bin}/mat --version")
     assert_match "usage:", shell_output("#{bin}/mat --help")
 
     (testpath/"hello.txt").write("hello world\n")
