168b69b1961f6d661b54ab466549647434f276b8
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-29 20:01:20 -0400

    Add facsimile 0.11.0

---
+++ Formula/facsimile.rb
@@ -0,0 +1,29 @@
+class Facsimile < Formula
+  desc "Terminal text editor written in Fortran with VSCode-style keybindings"
+  homepage "https://github.com/FortranGoingOnForty/facsimile"
+  url "https://github.com/FortranGoingOnForty/facsimile/archive/refs/tags/v0.11.0.tar.gz"
+  sha256 "bf4c4c7b68868356d4fb14040c1990f1d89146d5bc79cfc29de93b4e1f83d104"
+  license "MIT"
+  head "https://github.com/FortranGoingOnForty/facsimile.git", branch: "trunk"
+
+  depends_on "gcc" # for gfortran
+
+  def install
+    # Build using make
+    system "make"
+
+    # Install binary
+    bin.install "fac"
+
+    # Create symlink for convenience
+    bin.install_symlink "fac" => "facsimile"
+
+    # Install documentation
+    doc.install "README.md"
+  end
+
+  test do
+    # Test that fac can run (basic version check or help)
+    system "#{bin}/fac", "--help" rescue true
+  end
+end
