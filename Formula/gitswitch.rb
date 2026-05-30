a7039b26be314c86276ec9ce1ee7fc8bacc43c61
mfwolffe <wolffemf@dukes.jmu.edu>
2026-05-29 22:19:07 -0400

    gitswitch: 1.7.1

--- Formula/gitswitch.rb
+++ Formula/gitswitch.rb
@@ -1,8 +1,8 @@
 class Gitswitch < Formula
   desc "Secure Git identity and SSH/GPG key management for seamless account switching"
   homepage "https://github.com/tenseleyFlow/gitswitchC"
-  url "https://github.com/tenseleyFlow/gitswitchC/archive/refs/tags/v1.7.0.tar.gz"
-  sha256 "0276f02d346d300780753a7b279ab4558d142ff5739d23805ec175ffd3e1d5e8"
+  url "https://github.com/tenseleyFlow/gitswitchC/archive/refs/tags/v1.7.1.tar.gz"
+  sha256 "ba82b2e04847f49b77d549f5e5bbbcf889a5002f699148e4304b60014ac4ff2c"
   license "GPL-3.0-or-later"
   head "https://github.com/tenseleyFlow/gitswitchC.git", branch: "trunk"
 
