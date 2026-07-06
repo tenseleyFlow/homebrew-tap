a50f132f2555ceddcd643b44b989e76ee96230ac
mfwolffe <mfwolffe@outlook.com>
2026-07-05 20:39:01 -0400

    tally 0.1.1

--- Formula/tally.rb
+++ Formula/tally.rb
@@ -1,8 +1,8 @@
 class Tally < Formula
   desc "GNU wc reimplementation: byte-identical output, SIMD word counting"
   homepage "https://github.com/tenseleyFlow/tally"
-  url "https://github.com/tenseleyFlow/tally/releases/download/v0.1.0/tally-0.1.0.tar.gz"
-  sha256 "8ed3e585500bd0091fcb32beae097d84a3d1b304de46e5b51796d1b976c6358f"
+  url "https://github.com/tenseleyFlow/tally/releases/download/v0.1.1/tally-0.1.1.tar.gz"
+  sha256 "65eb5860c9e07b217f212fcec2301ef9d30e3b649b7315632b075ebadb051ddd"
   license "GPL-3.0-or-later"
   head "https://github.com/tenseleyFlow/tally.git", branch: "trunk"
 
